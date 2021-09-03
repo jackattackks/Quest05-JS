7b1c14dffc0e8a9232eeae216e5f7afe7b0d84d9
jacqueline ocana <ocana_j@qwasar.io>
2021-09-03 00:17:34 +0000

    create function that takes in param and returns value depending on if negative or not

---
+++ ex01/my_is_negative.js
@@ -0,0 +1,16 @@
+
+function my_is_negative(n){
+
+    if (n < 0 ){
+        // if the number is negative it will return 0
+        return 0;
+    }else{
+        //if the number is 0 or positive it will return 1
+        return 1;
+    }
+}
+
+//test cases provided will print out
+// console.log(my_is_negative(-1)); //should return 0
+// console.log(my_is_negative(1)); // should return 1
+// console.log(my_is_negative(0)); // should return 1
