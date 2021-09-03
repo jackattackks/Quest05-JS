d7667c2f3b4f941a1706559246025eb9672ed956
jacqueline ocana <ocana_j@qwasar.io>
2021-09-03 01:08:04 +0000

    output a composed string that takes in 2 strings and 1 number as parameters

--- ex05/my_mult.js
+++ ex05/my_mult.js
@@ -1,4 +1,4 @@
-//subtracts two params and returns their difference
+//multiply two params and returns their product
 function my_mult(nbr1, nbr2){
     return nbr1 * nbr2;
 }
---
+++ ex06/my_string_formatting.js
@@ -0,0 +1,9 @@
+
+//take in strings as parameters and output
+
+function my_string_formatting(firstName = "", lastName = "" , age) {
+    //concatanate variables with the sentence
+    console.log("Hello, my name is " + firstName + " " + lastName + ", I'm " + age + ".");
+
+};
+
